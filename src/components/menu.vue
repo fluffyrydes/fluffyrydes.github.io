65fed7d6d3dceef36d06f1c3bed391434f732c34
Hannah Wendland <hwen@netlight.com>
2021-01-30 14:50:27 +0100

    fix links

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,13 +1,13 @@
 <template>
-    <header :class >
+    <header >
 
         <div v-if="extraHeader">
             <div class="site-logo" :class="logoColor" >Wild Rydes  </div>
-             
+
             <div class="row column medium-8 large-6 xlarge-5 xxlarge-4">
             <h1 class="">{{title}}</h1>
             <p class="content">
-                {{content}} 
+                {{content}}
             </p>
             </div>
         </div>
@@ -15,18 +15,17 @@
         <h1  v-show="showTitle" class="title">Wild Rydes</h1>
         <nav  id="nav"  v-show="isOpen" class="site-nav">
             <ul>
-                <li><router-link to="/">Home</router-link></li> 
-                <li><router-link to="/unicorns">Meet the Unicorns</router-link></li> 
-                <li><router-link to="/investors">Investors &amp; Board of Directors</router-link></li> 
-                <li><router-link to="/faq">FAQ</router-link></li> 
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/unicorns">Meet the Unicorns</router-link></li>
+                <li><router-link to="/investors">Investors &amp; Board of Directors</router-link></li>
+                <li><router-link to="/faq">FAQ</router-link></li>
                 <li> <router-link to="/apply">Apply</router-link> </li>
-                <li> <a href="/auth">Register</a> </li>
             </ul>
         </nav>
         <div :class="menuStatus">
             <button @click='toggle()' type="button" class="btn-menu"><span>Menu</span></button>
         </div>
-        <a  v-show="showTitle" class="home-button" href="/auth">Giddy Up!</a>
+        <button v-show="showTitle" class="home-button">Giddy Up!</button>
     </header>
 </template>
 
@@ -36,12 +35,12 @@ export default {
   props: ['showTitle','extraHeader','title', 'content','logoColor'],
   data () {
       return {
-            isOpen: false, 
+            isOpen: false,
             menuStatus:'',
       }
     },
   methods: {
-    toggle: function () {  
+    toggle: function () {
        this.isOpen = !this.isOpen
        if(!this.menuStatus){
         this.menuStatus="menu-opened"
