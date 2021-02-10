d93413ac1b639a4a939be2dab9779549921017ed
Hannah Wendland <hwen@netlight.com>
2021-02-10 07:29:03 +0100

    use meeting-form component

--- src/components/itinerary.vue
+++ src/components/itinerary.vue
@@ -1,18 +1,14 @@
 <template>
-  <form>
-    <div class="row" v-for="meeting in meetings" :key="meeting.id">
-      <label>
-        <span class="time blink">{{ meeting.time }}</span>
-        <span class="destination">{{ meeting.label }}</span>
-        <input type="text" :placeholder="buildPlaceholder(meeting.id)" v-model="meeting.input">
-      </label>
-    </div>
+  <form v-on:submit.prevent>
+    <meeting-form class="row" v-for="meeting in meetings" :meeting="meeting" :key="meeting.id"/>
   </form>
 </template>
 
 <script>
+import MeetingForm from "@/components/meeting-form";
 export default {
   name: "itinerary",
+  components: { MeetingForm },
   data () {
     return {
       meetings: [
@@ -21,70 +17,29 @@ export default {
           id: "ZpE3MDVhZjYtZmUxMC00OTgyLWIwNGUtNzRjNzZjZWE1OGF",
           input: "",
           time: "17.45",
-          isCorrect: false
         },
         {
           label: "Ryde to your next assignment",
           id: "ZjEoMDVhZjYtZmUxMC00OTgyLWIwNGUtNzRjNzZjZWE1OGF",
           input: "",
           time: "18.15",
-          isCorrect: false
         },
         {
           label: "Ryde to feedback land",
           id: "ZjE3MDVhZjYtZmUxMC00OTgyLWIwNGUtNzRjNzZjZWE1OGF",
           input: "",
           time: "19.00",
-          isCorrect: false
         },
       ]
     }
   },
   methods: {
-   buildTeamsLink: id => {
-     return "https://teams.microsoft.com/l/meetup-join/19%3ameeting_" + id + "m%40thread.v2"
-  },
-    buildPlaceholder: id => {
-     return "Meeting ID: " + id.slice(0, 8) + "🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄"
-    }
   }
 }
 </script>
 
 <style scoped>
-form input {
-  background: #333;
-  border: 1px solid transparent;
-  color: #fff;
-  border-radius: 40px;
-  height: 3.4375rem;
-  padding: 1.875rem;
-  margin: 0.75rem;
-  outline: none;
-  width: 100%
-}
-
 form {
   margin-bottom: 7rem;
-}
-
-label .destination {
-  font-weight: bold;
-}
-
-label .time {
-  color: deepskyblue;
-  font-weight: bold;
-  margin-right: 10px;
-}
-
-.blink {
-  animation: blinker 0.5s linear infinite;
-}
-
-@keyframes blinker {
-  50% {
-    opacity: 0;
-  }
 }
 </style>
