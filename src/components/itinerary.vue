d9a6625a489b22a277bae4fcb199a27dbca6b5ef
Hannah Wendland <hwen@netlight.com>
2021-02-10 19:26:40 +0100

    put meetings info into store

--- src/components/itinerary.vue
+++ src/components/itinerary.vue
@@ -6,35 +6,12 @@
 
 <script>
 import MeetingForm from "@/components/meeting-form";
+import { mapState } from 'vuex'
+
 export default {
   name: "itinerary",
   components: { MeetingForm },
-  data () {
-    return {
-      meetings: [
-        {
-          label: "Ryde to more professional charisma",
-          id: "ZpE3MDVhZjYtZmUxMC00OTgyLWIwNGUtNzRjNzZjZWE1OGF",
-          input: "",
-          time: "17.45",
-        },
-        {
-          label: "Ryde to your next assignment",
-          id: "ZjEoMDVhZjYtZmUxMC00OTgyLWIwNGUtNzRjNzZjZWE1OGF",
-          input: "",
-          time: "18.15",
-        },
-        {
-          label: "Ryde to feedback land",
-          id: "ZjE3MDVhZjYtZmUxMC00OTgyLWIwNGUtNzRjNzZjZWE1OGF",
-          input: "",
-          time: "19.00",
-        },
-      ]
-    }
-  },
-  methods: {
-  }
+  computed: mapState(['meetings'])
 }
 </script>
 
